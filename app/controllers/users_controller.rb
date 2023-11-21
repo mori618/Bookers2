e2aa86a5dace189710de7d89416a068921e5f538
mori618 <148190402+mori618@users.noreply.github.com>
2023-11-21 14:00:27 +0000

    finish11/21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "successfully"
       redirect_to user_path(@user.id)
     else
     render :edit
@@ -45,7 +46,7 @@ class UsersController < ApplicationController
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(current_user.id)
+      redirect_to books_path
     end
   end
 end
